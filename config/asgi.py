41f84215037bb87b7c2ad7dc410900abb2240f1a
Victor Arthur Coningham <va.coningham@gmail.com>
2022-05-27 12:57:50 +0800

    FEAT: Channels consumer created.

--- config/asgi.py
+++ config/asgi.py
@@ -1,7 +1,9 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+import client.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -9,4 +11,9 @@ asgi_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application()
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            client.routing.websocket_urlpatterns
+        )
+    ),
 })
--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-ASGI_APPLICATION = "config.asgi.application"
+ASGI_APPLICATION = 'config.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
